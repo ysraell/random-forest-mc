660df50d344de245e991f05f34aca7b2747148df
Israel de Oliveira <israel.de.oliveira@hp.com>
2022-07-31 16:03:53 -0300

    Add extractor script for bziped2 datasets.

--- tests/extract_datasets.sh
+++ tests/extract_datasets.sh
@@ -6,7 +6,7 @@ mkdir -p /tmp/datasets/
 for bziped2 in `ls tests/datasets/*.bz2`;
     do
         csved=`echo $bziped2 | sed -e 's/\.bz2//g' | sed -e 's;tests\/datasets\/;;g'`
-        bunzip -k -c $bziped2 >/tmp/datasets/${csved}
+        bunzip2 -k -c $bziped2 >/tmp/datasets/${csved}
     done
 
 
