481be088f04a38f57974aebbc0f4a701daa215d7
Israel Gonçalves de Oliveira <Israel de Oliveira>
2024-03-17 11:58:54 -0300

    wip

--- src/random_forest_mc/utils.py
+++ src/random_forest_mc/utils.py
@@ -25,19 +25,25 @@ def dump_file_json(path: DictsPathType, var: Any):
 
 
 class LoadDicts:
-    def __init__(self, dict_path: DictsPathType = "./data"):
+    def __init__(self, dict_path: DictsPathType = "./data",  ignore_errors: bool = False):
         Dicts_glob = Path().glob(f"{dict_path}/*.json")
         self.List = []
         self.Dict = {}
         self.not_attr = []
         for path_json in Dicts_glob:
-            name = path_json.as_posix().split("/")[-1].replace(".json", "")
-            self.List.append(name)
-            self.Dict[name] = load_file_json(path_json)
-            if name.isidentifier() and not iskeyword(name):
-                setattr(self, name, self.Dict[name])
-            else:
-                self.not_attr.append(name)
+            try:
+                name = path_json.as_posix().split("/")[-1].replace(".json", "")
+                self.List.append(name)
+                self.Dict[name] = load_file_json(path_json)
+                if name.isidentifier() and not iskeyword(name):
+                    setattr(self, name, self.Dict[name])
+                else:
+                    self.not_attr.append(name)
+            except Exception as e:
+                print(f"Error trying to load the file: {path_json.absolute()}: ")
+                if not ignore_errors:
+                    raise e
+                print(e)                    
 
     def __repr__(self) -> str:
         return "LoadDicts: {}".format(", ".join(self.List))
