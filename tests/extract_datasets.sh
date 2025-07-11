43d64a9b4f80c6ef63af7732e925d439c843621d
Israel de Oliveira <prof.israel@gmail.com>
2025-07-11 19:41:38 -0300

    WIP

--- tests/extract_datasets.sh
+++ tests/extract_datasets.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 set -e
 
-mkdir -p /tmp/datasets/
+TMP_DIR=$1
+
+if [ -z "$TMP_DIR" ]; then
+    echo "Usage: $0 <tmp_dir>"
+    exit 1
+fi
+
+DATASETS_DIR=$TMP_DIR/datasets/
+mkdir -p $DATASETS_DIR
 
 for bziped2 in `ls tests/datasets/*.bz2`;
     do
         csved=`echo $bziped2 | sed -e 's/\.bz2//g' | sed -e 's;tests\/datasets\/;;g'`
-        bunzip2 -k -c $bziped2 >/tmp/datasets/${csved}
+        bunzip2 -k -c $bziped2 >$DATASETS_DIR/${csved}
     done
 
-mkdir -p /tmp/datasets/load_json_empty
-echo "" >/tmp/datasets/load_json_empty/empty.json
+mkdir -p $DATASETS_DIR/load_json_empty
+echo "" >$DATASETS_DIR/load_json_empty/empty.json
 
-mkdir -p /tmp/datasets/load_json_csv_like
-echo 'a,"!WE@",3' >/tmp/datasets/load_json_csv_like/csv_like.json
+mkdir -p $DATASETS_DIR/load_json_csv_like
+echo 'a,"!WE@",3' >$DATASETS_DIR/load_json_csv_like/csv_like.json
 
-mkdir -p /tmp/datasets/load_json_keyword
-echo '{"a": 1}' >/tmp/datasets/load_json_keyword/True.json
+mkdir -p $DATASETS_DIR/load_json_keyword
+echo '{"a": 1}' >$DATASETS_DIR/load_json_keyword/True.json
 
 #EOF
