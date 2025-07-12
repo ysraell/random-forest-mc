4a7be8597647c86dc68d44f4589f61a4bbc78f59
Israel de Oliveira <prof.israel@gmail.com>
2025-07-11 22:40:38 -0300

    WIP

--- tests/run_tests_all_versions.sh
+++ tests/run_tests_all_versions.sh
@@ -11,3 +11,33 @@ do
     pip uninstall -y random-forest-mc
     bash -x tests/run_tests.sh || echo "Something wrong with $version"
 done
+
+# with JIT:
+for version in `cat tests/versions-jit-nogil.txt |grep -v '#'`;
+do
+    export PYENV_VERSION_SUFFIX="-jit" 
+    export PYTHON_CONFIGURE_OPTS='--enable-experimental-jit'
+    pyenv install -f $version
+    pyenv local $version$PYENV_VERSION_SUFFIX
+    pip install -U -r tests/requirements.txt
+    pip uninstall -y random-forest-mc
+    bash -x tests/run_tests.sh || echo "Something wrong with $version-$PYENV_VERSION_SUFFIX"
+
+    export PYTHON_GIL=0
+
+    export PYENV_VERSION_SUFFIX="-nogil" 
+    export PYTHON_CONFIGURE_OPTS='--disable-gil'
+    pyenv install -f $version
+    pyenv local $version$PYENV_VERSION_SUFFIX
+    pip install -U -r tests/requirements.txt
+    pip uninstall -y random-forest-mc
+    bash -x tests/run_tests.sh || echo "Something wrong with $version-$PYENV_VERSION_SUFFIX"
+
+    export PYENV_VERSION_SUFFIX="-nogil-jit" 
+    export PYTHON_CONFIGURE_OPTS='--disable-gil --enable-experimental-jit'
+    pyenv install -f $version
+    pyenv local $version$PYENV_VERSION_SUFFIX
+    pip install -U -r tests/requirements.txt
+    pip uninstall -y random-forest-mc
+    bash -x tests/run_tests.sh || echo "Something wrong with $version-$PYENV_VERSION_SUFFIX"
+done
